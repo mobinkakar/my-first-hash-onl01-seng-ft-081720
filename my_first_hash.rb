7584a5af7525ee00c766697142da36e203d6afde
Mobin Kakar <mobin.kakar@wsabronx.org>
2020-09-01 16:56:31 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -17,7 +17,11 @@ end
 def adding
   shipping_manifest = {"whale bone corsets" => 5, "porcelain vases" => 2, "oil paintings" => 3}
 
+<<<<<<< HEAD
   shipping_manifest["muskets"] = 2
   shipping_manifest["gun powder"] = 4
+=======
+  shipping_manifest["pearl necklace"] = 1
+>>>>>>> c7b5e547df931ee62fff1e12abbc35b8c9ac0a5f
   shipping_manifest
 end
